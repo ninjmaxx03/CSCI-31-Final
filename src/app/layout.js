7c4ddbfe6fa2a9dc2be06db55ceccabea52c939c
ninjamaxx03 <ninjamaxx03@gmail.com>
2024-04-08 14:02:59 -0700

    Add Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics/>
         <Navbar />
         {children}
         <Footer />
