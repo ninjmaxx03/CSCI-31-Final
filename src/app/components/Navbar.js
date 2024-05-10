578cf2d8c319cb1636e739f836f77b64d855dc07
ninjamaxx03 <maxgeerheads@gmail.com>
2024-05-09 22:32:20 -0700

    Dynamic Pages

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,6 @@
 import Button from './Button'
 import Tag from './Tag'
-import { CodeBracketIcon } from '@heroicons/react/24/solid'
+import { ServerStackIcon } from '@heroicons/react/24/solid'
 
 const navigationButtons = [
   {
@@ -19,3 +19,30 @@ const navigationButtons = [
     isNew: false,
   },
 ]
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between bg-indigo-50 p-6 items-center sticky top-0 shadow-lg">
+      <a href = "/" className=" p-6 bg-red rounded-full hover:scale-100 transition-all cursor-pointrer">
+        <ServerStackIcon className="h-14 w-14 text-stone-400" />
+        </a>
+      <div className="flex gap-x-4 items-center">
+        {navigationButtons.map(({ href,title, isNew}) => (
+          <>
+
+
+        <Button href = {href} classNameProp="flex gap-2 text-xl">
+
+        {isNew && <Tag classNames='mr-2 animate-bounce'/>}
+
+          {title}
+
+          </Button>
+
+        </>
+
+        ))}
+      </div>
+    </nav>
+  )
+}
